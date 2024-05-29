859cbdd6228708cd9996c147fc06b44bad35bd8f
bender <mikitabondarkov@gmail.com>
2024-05-30 02:47:11 +0300

    fix database

--- migrations/Version20240528232617.php
+++ migrations/Version20240528232617.php
@@ -25,6 +25,7 @@ final class Version20240528232617 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
 
+        $this->addSql('CREATE SEQUENCE "items_collection_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE items_collection (id INT NOT NULL, collection_category_id INT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(50) NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX idx_62555b05a76ed395 ON items_collection (user_id)');
         $this->addSql('CREATE INDEX idx_62555b058eec0163 ON items_collection (collection_category_id)');
@@ -33,6 +34,7 @@ final class Version20240528232617 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
 
+        $this->addSql('CREATE SEQUENCE "item_attribute_value_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE item_attribute_value (id INT NOT NULL, custom_item_attribute_id INT NOT NULL, item_id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX idx_448e6020126f525e ON item_attribute_value (item_id)');
         $this->addSql('CREATE INDEX idx_448e60208bf3b7b6 ON item_attribute_value (custom_item_attribute_id)');
@@ -41,12 +43,14 @@ final class Version20240528232617 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
 
+        $this->addSql('CREATE SEQUENCE "collection_category_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE collection_category (id INT NOT NULL, name VARCHAR(45) NOT NULL, PRIMARY KEY(id))');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\PostgreSQL100Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
 
+        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, status SMALLINT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX uniq_identifier_email ON "user" (email)');
         $this->abortIf(
@@ -54,6 +58,7 @@ final class Version20240528232617 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
 
+        $this->addSql('CREATE SEQUENCE "comments_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE comments (id INT NOT NULL, username_id INT DEFAULT NULL, item_id INT DEFAULT NULL, content VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX idx_5f9e962a126f525e ON comments (item_id)');
         $this->addSql('CREATE INDEX idx_5f9e962aed766068 ON comments (username_id)');
@@ -62,6 +67,7 @@ final class Version20240528232617 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
 
+        $this->addSql('CREATE SEQUENCE "item_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE item (id INT NOT NULL, item_collection_id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX idx_1f1b251ebde5fe26 ON item (item_collection_id)');
         $this->abortIf(
@@ -81,6 +87,7 @@ final class Version20240528232617 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
 
+        $this->addSql('CREATE SEQUENCE "custom_item_attribute_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE custom_item_attribute (id INT NOT NULL, item_collection_id INT NOT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX idx_dc45ccd1bde5fe26 ON custom_item_attribute (item_collection_id)');
         $this->abortIf(
@@ -88,6 +95,7 @@ final class Version20240528232617 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
 
+        $this->addSql('CREATE SEQUENCE "item_tag_item_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE item_tag_item (item_tag_id INT NOT NULL, item_id INT NOT NULL, PRIMARY KEY(item_tag_id, item_id))');
         $this->addSql('CREATE INDEX idx_9d195263126f525e ON item_tag_item (item_id)');
         $this->addSql('CREATE INDEX idx_9d1952633c2b16de ON item_tag_item (item_tag_id)');
@@ -96,6 +104,7 @@ final class Version20240528232617 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
 
+        $this->addSql('CREATE SEQUENCE "item_tag_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE item_tag (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX item_idx ON item_tag (name)');
     }
