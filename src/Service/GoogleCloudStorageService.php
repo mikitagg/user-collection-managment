96eb87aeb39315d558f361da124ca4df99b09ab1
bender <mikitabondarkov@gmail.com>
2024-05-31 02:14:42 +0300

    gcs update

--- src/Service/GoogleCloudStorageService.php
+++ src/Service/GoogleCloudStorageService.php
@@ -18,7 +18,7 @@ class GoogleCloudStorageService
             "type" => "service_account",
             "project_id" => "bold-column-424118-i9",
             "private_key_id" => "9d1578163f205c447c12047b8e8782f27822f8cb",
-            "private_key" => getenv("GOOGLE_CLOUD_PRIVATE_KEY"),
+            "private_key" => getenv('GOOGLE_CLOUD_PRIVATE_KEY'),
             "client_email" => "mikitabondarkou@bold-column-424118-i9.iam.gserviceaccount.com",
             "client_id" => "101092736103622014913",
             "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
@@ -32,6 +32,7 @@ class GoogleCloudStorageService
             'projectId' => "bold-column-424118-i9",
             'keyFile' => $keyFile,
         ]);
+        dd(getenv("GOOGLE_CLOUD_PRIVATE_KEY"));
         $this->bucketName = "mikitaimagebucket";
     }
     public function uploadImage($file, $fileName)
