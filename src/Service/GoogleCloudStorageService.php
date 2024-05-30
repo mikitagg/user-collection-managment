390736608590124237b8b8a2884c6c1598b0a9f4
bender <mikitabondarkov@gmail.com>
2024-05-31 01:52:13 +0300

    gcs update

--- src/Service/GoogleCloudStorageService.php
+++ src/Service/GoogleCloudStorageService.php
@@ -13,27 +13,25 @@ class GoogleCloudStorageService
 
     public function __construct()
     {
-        $keyFile = [
-            "type" => "service_account",
-            "project_id" => getenv('GOOGLE_CLOUD_PROJECT_ID'),
-            "private_key_id" => getenv('GOOGLE_CLOUD_PRIVATE_KEY_ID'),
-            "private_key" => getenv('GOOGLE_CLOUD_PRIVATE_KEY'),
-            "client_email" => "mikitabondarkou@bold-column-424118-i9.iam.gserviceaccount.com",
-            "client_id" => getenv('GOOGLE_CLOUD_CLIENT_ID'),
-            "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
-            "token_uri" => "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url" => getenv('GOOGLE_CLOUD_AUTH_PROVIDER'),
-            "client_x509_cert_url" => getenv('GOOGLE_CLOUD_CERT_URL'),
-            "universe_domain" => "googleapis.com"
-        ];
-
-        dd(getenv('GOOGLE_APPLICATION_CREDENTIALS'));
         $this->storageClient = new StorageClient([
             'projectId' => getenv('GOOGLE_CLOUD_PROJECT_ID'),
-            'keyFile' => $keyFile,
+            'keyFile' => [
+                    "type"=> "service_account",
+                    "project_id"=> getenv('GOOGLE_CLOUD_PROJECT_ID'),
+                    "private_key_id"=> getenv('GOOGLE_CLOUD_PRIVATE_KEY_ID'),
+                    "private_key"=> getenv('GOOGLE_CLOUD_PRIVATE_KEY'),
+                    "client_email"=> "mikitabondarkou@bold-column-424118-i9.iam.gserviceaccount.com",
+                    "client_id"=> "101092736103622014913",
+                    "auth_uri"=> "https://accounts.google.com/o/oauth2/auth",
+                    "token_uri"=> "https://oauth2.googleapis.com/token",
+                    "auth_provider_x509_cert_url"=> getenv('GOOGLE_CLOUD_AUTH_PROVIDER'),
+                    "client_x509_cert_url"=> getenv('GOOGLE_CLOUD_CERT_URL'),
+                    "universe_domain"=> "googleapis.com"
+            ],
             ]);
-        $this->bucketName = getenv('GOOGLE_CLOUD_STORAGE_BUCKET');
+        $this->bucketName = "mikitaimagebucket";
     }
+
     public function uploadImage($file, $fileName)
     {
         $bucket = $this->storageClient->bucket($this->bucketName);
