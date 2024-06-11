9c2d220bccd92fa6a1d3070d4ec47970581e2292
bender <mikitabondarkov@gmail.com>
2024-06-11 18:51:24 +0300

    jira integration

---
+++ src/Controller/JiraController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Service\JiraService;
+
+class JiraController extends AbstractController
+{
+    private $jiraService;
+    private PaginatorInterface $paginator;
+
+    public function __construct(JiraService $jiraService, PaginatorInterface $paginator)
+    {
+        $this->jiraService = $jiraService;
+        $this->paginator = $paginator;
+    }
+
+
+    #[Route('/jira/issue/show', name: 'jira_issue_show')]
+    public function showIssues(Request $request): Response
+    {
+        $data = $this->jiraService->getUserIssues($this->getUser()->getEmail());
+
+        $pagination = $this->paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            5
+        );
+
+        return $this->render('jira/show.html.twig', [
+            'pagination' => $pagination
+        ]);
+    }
+
+
+    /**
+     * @throws GuzzleException
+     */
+    #[Route('/jira/ticket', name: 'jira_ticket')]
+    public function createTicket(Request $request): Response
+    {
+        $user = $this->jiraService->getUserByEmail($this->getUser()->getEmail());
+
+        if(!$user)
+        {
+            $user = $this->jiraService->createUser();
+        }
+
+        $url = $request->server->get('HTTP_REFERER');
+        $collection = $request->request->get('collection');
+
+        $response = $this->jiraService->createTicket($request->request->all(), $url, $collection, $user);
+        $key = $this->jiraService->getJiraKey($response);
+        $link = $this->jiraService->generateLink($key);
+        return new Response($response);
+    }
+
+}
---
+++ src/Service/JiraService.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Service;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class JiraService
+{
+    private Client $client;
+
+
+    public function __construct(private readonly Security $security)
+    {
+        $this->client = new Client([
+            'base_uri' => getenv('BASE_URI'),
+            'auth' => ['n21601201@gmail.com', getenv('AUTH_TOKEN')],
+        ]);
+    }
+
+    public function getUserIssues($user): array
+    {
+        $response = $this->client->get("search?jql=reporter=".$this->getUserByEmail($user));
+        $response = json_decode($response->getBody()->getContents(), true);
+        return $response['issues'];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getUserByEmail(string $email)
+    {
+        $response = $this->client->get('user/search?query='.$email);
+        $data = json_decode($response->getBody(), true);
+        if($data)
+        {
+            return $data[0]['accountId'];
+        }
+        return null;
+    }
+
+    public function generateLink($key)
+    {
+        return 'https://bondarkov.atlassian.net/browse/'.$key;
+    }
+
+    public function createTicket(array $request, string $url, ?int $collection, string $user) {
+
+        $response = $this->client->post('issue', [
+            'json' => [
+                'fields' => [
+                    'project' => [
+                        'key' => 'KAN'
+                    ],
+                    'summary' => $request['name'],
+                    'description' => $request['description'],
+                    'issuetype' => [
+                        'name' => 'Task'
+                    ],
+                    'reporter' => [
+                        'accountId' => $user,
+                    ],
+                    'priority' => [
+                        'id' => $request['priority'],
+                    ],
+                    'customfield_10034' => $url ,
+                    'customfield_10035' => $collection,
+                    ]
+                ]
+            ]);
+
+        return $response->getBody();
+    }
+
+    public function getJiraKey($response)
+    {
+        $response = json_decode($response->getContents(), true);
+        return $response['key'];
+    }
+
+    public function createUser() {
+
+        $user = $this->security->getUser();
+        $response = $this->client->post('user', [
+            'json' => [
+                'emailAddress' => $user->getEmail(),
+                'products' => ['jira-software']
+            ],
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+
+        return $data['accountId'];
+    }
+}
